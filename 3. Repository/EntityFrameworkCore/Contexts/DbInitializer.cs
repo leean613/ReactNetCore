c0806a12ab77b985c2e9c6071098cf6f3fe8d1e9
binhnguyen <binhnd@glotechvn.com>
2020-10-21 17:12:36 +0700

    add auto migration

---
+++ 3. Repository/EntityFrameworkCore/Contexts/DbInitializer.cs
@@ -0,0 +1,39 @@
+﻿using Entities.React;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EntityFrameworkCore.Contexts
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(ReactDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any students.
+            if (context.Users.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Users.Add(new User
+            {
+                Id = Guid.NewGuid(),
+                CreateDate = DateTime.Now,
+                UserName = "admin",
+                Password = EncryptPassword("1"),
+                Role = 1,
+            });
+
+            context.SaveChanges();
+        }
+
+        public static string EncryptPassword(string passsword)
+        {
+            return Convert.ToBase64String(SHA1.Create().ComputeHash(Encoding.ASCII.GetBytes(passsword)));
+        }
+    }
+}
--- 4. Entity/Entities/ReactConfigurations/UserConfiguration.cs
+++ 4. Entity/Entities/ReactConfigurations/UserConfiguration.cs
@@ -1,9 +1,6 @@
 ﻿using Entities.React;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Entities.ReactConfigurations
 {
